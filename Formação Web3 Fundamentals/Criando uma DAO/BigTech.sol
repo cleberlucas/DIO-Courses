4a6943e612f1acc80b1d5831ab5a34d405908475
Cleber Lucas <clebersiber@hotmail.com>
2024-02-03 15:21:15 -0300

    Formação Web3 Fundamentals -> Criando uma DAO

--- Formação Web3 Fundamentals/Criando uma DAO/BigTech.sol
+++ Formação Web3 Fundamentals/Criando uma DAO/BigTech.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract DecentralizaTechDAO {
+contract BigTechDAO {
     address public dono;
     uint public totalAcoes;
     mapping(address => uint) public acoes;
